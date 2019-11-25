6466ada1c4eacc6d06d6a60dceb0acd6afc6ac97
Adrian Mindek <adrian.mindek@gmail.com>
2019-11-25 15:50:50 +0100

    AddedMusic and Animation for Play button

---
+++ app/src/main/java/com/example/doodlejump/GameActivity.java
@@ -0,0 +1,14 @@
+package com.example.doodlejump;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+    }
+
+}
